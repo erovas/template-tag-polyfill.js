cbe13387b266d22a972ba062e211ba490da617a4
erovas <emarovas2@gmail.com>
2021-01-05 23:10:36 +0100

    change Readme, pull js

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # template-tag-polyfill.js
-Full functionality polyfill for template tag (ie11+)
+(EN) Full functionality polyfill for template> tag.
+(ES) Polyfill con funcionalidad completa para etiqueta template.
+
+## Compatibility - Compatibilidad
+
+(EN) For internet explorer 11+.
+(ES) Para internet explorer 11+.
+
+## How to use? - ¿Cómo utilizarlo?
+
+(EN) This polyfill must be put in the head tag of html document.
+(ES) Este polyfill debe ser puesta en la etiqueta head  del documento html.
+
+``` html
+<html>
+    <head>
+        <title> My page - Mi página <title>
+        <script src="template-tag-polyfill.js"></script>
+    </head>
+    <body>
+        <!-- page content - contenido de la página -->
+    </body>
+</html>
+```
+
+## API
+(EN) The same as modern browsers
+(ES) La misma que los navegadores modernos
+
+## Authors - Autores
+
+* **Emanuel Rojas Vásquez** - *Initial work* - [erovas](https://github.com/erovas)
+
+## License - Licencia
+
+(EN) This project is licensed under the BSD 3-Clause License - see the [LICENSE.md](LICENSE.md) file for details.
+(ES) Este proyecto está licenciado bajo Licencia BSD 3-Clause - ver el archivo [LICENSE.md](LICENSE.md) para mas detalles.
---
+++ template-tag-polyfill.js
@@ -0,0 +1,134 @@
+/**
+ * template-tag-polyfill.js v1.0.0
+ * Polyfill de <template> que intenta darle toda la funcionalidad nativa de este elemento en navegadores que NO lo soportan
+ * [Back-compatibility: IE11+]
+ * Copyright (c) 2021, Emanuel Rojas Vásquez
+ * BSD 3-Clause License
+ * https://github.com/erovas/template-tag-polyfill.js
+ */
+if(!('content' in document.createElement('template'))){
+
+    //#region Estilo para ocultar contenido de los templates
+    
+    let style = document.createElement('style');
+    style.innerHTML = 'template { display: none; }';
+    document.head.appendChild(style);
+    style = undefined;
+
+    //#endregion
+
+    //Para emular el instanceof y la interfaz del objeto nativo
+    window.HTMLTemplateElement = window.HTMLElement;
+
+    const div = document.createElement('div');
+    const TemplateTagName = 'TEMPLATE';
+    const innerHTMLText = '-_-innerHTMLText';
+    const innerHTML = Object.getOwnPropertyDescriptor(Element.prototype, 'innerHTML') || Object.getOwnPropertyDescriptor(HTMLElement.prototype, 'innerHTML');
+    const createElement = Document.prototype.createElement;
+    let templates = document.getElementsByTagName('template');
+
+
+    const _renderTemplate = function(tag){
+
+        //El template ya esta renderizado
+        if(tag.content)
+            return;
+
+        //Se genera un pseudo innerHTML
+        tag[innerHTMLText] = innerHTML.get.call(tag);
+
+        const child = tag.childNodes;
+        const fragment = document.createDocumentFragment();
+
+        while(child[0])
+            fragment.appendChild(child[0]);
+            
+        tag.content = fragment;
+    }
+
+    //#region Renderizado de los templates existentes en el documento durante el parseo del mismo
+
+    let mutation = new MutationObserver(function(mtns){
+        //Quizas hay templates dentro de los templates
+        const tls = [].slice.call(templates);
+
+        for (let i = 0; i < tls.length; i++)
+            _renderTemplate(tls[i]);
+    });
+
+    mutation.observe(document.getElementsByTagName('html')[0], { childList: true, subtree: true });
+
+    window.addEventListener('load', function(){
+        mutation.disconnect();
+        mutation = undefined;
+        templates = undefined;
+    }, { once: true });
+
+    //#endregion
+
+    //#region Redefinicion de innerHTML
+
+    [Element.prototype, HTMLElement.prototype].forEach(function(item){
+        Object.defineProperty(item, 'innerHTML', {
+            get: function(){
+                //Devuelve el pseudo innerHTML
+                if(this.tagName === TemplateTagName)
+                    return this[innerHTMLText];
+                    
+                return innerHTML.get.call(this);
+            },
+            set: function(value){
+
+                if(this.tagName === TemplateTagName){
+                    //Se generan los nodos y con ello el nuevo pseudo innerHTML
+                    div.innerHTML = value;
+                    this[innerHTMLText] = innerHTML.get.call(div);
+
+                    const content = this.content;
+                    const childAdd = div.childNodes;
+                    const childRemove = content.childNodes;
+
+                    //Se eliminan los nodos del content del template
+                    while(childRemove[0])
+                        content.removeChild(childRemove[0]);
+
+                    //Se agregan los nuevos nodos al content del template
+                    while(childAdd[0])
+                        content.appendChild(childAdd[0]);
+                }
+                else {
+                    innerHTML.set.call(this, value);
+
+                    //Si dentro del tag donde se ha hecho el inner hay algun template, se renderiza
+                    const tls = [].slice.call(this.getElementsByTagName(TemplateTagName.toLowerCase()));
+
+                    for (let i = 0; i < tls.length; i++)
+                        _renderTemplate(tls[i]);
+                }
+            }
+        });
+    });
+
+    //#endregion
+
+    //#region Redefinicion de createElement()
+
+    Object.defineProperty(Document.prototype, 'createElement', {
+        configurable: true,
+        enumerable: true,
+        writable: true,
+        value: function(tagName, options){
+
+            const tag = createElement.apply(this, arguments);
+
+            if(tag.tagName === TemplateTagName){
+                tag.content = this.createDocumentFragment();
+                tag[innerHTMLText] = '';
+            }
+
+            return tag;
+        }
+    });
+
+    //#endregion
+}
